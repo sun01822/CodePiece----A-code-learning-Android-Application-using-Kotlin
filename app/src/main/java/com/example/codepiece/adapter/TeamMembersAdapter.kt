e7ebd44b6ed10dd6c2f1db5e64c4d3a216a28a1c
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-08-21 00:17:27 +0600

    Version 3.2

--- app/src/main/java/com/example/codepiece/adapter/TeamMembersAdapter.kt
+++ app/src/main/java/com/example/codepiece/adapter/TeamMembersAdapter.kt
@@ -1,19 +1,17 @@
 package com.example.codepiece.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.codepiece.R
 import com.example.codepiece.data.TeamMember
+import com.example.codepiece.databinding.ItemTeamMemberBinding
 
 class TeamMembersAdapter(private val teamMembers: List<TeamMember>) :
     RecyclerView.Adapter<TeamMembersAdapter.TeamMemberViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TeamMemberViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_team_member, parent, false)
-        return TeamMemberViewHolder(view)
+        val binding = ItemTeamMemberBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TeamMemberViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: TeamMemberViewHolder, position: Int) {
@@ -25,11 +23,11 @@ class TeamMembersAdapter(private val teamMembers: List<TeamMember>) :
         return teamMembers.size
     }
 
-    inner class TeamMemberViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class TeamMemberViewHolder(private val binding: ItemTeamMemberBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(teamMember: TeamMember) {
-            itemView.imageView.setImageResource(teamMember.imageResId)
-            itemView.textName.text = teamMember.name
-            itemView.textDetails.text = teamMember.details
+            binding.imageView.setImageResource(teamMember.imageResId)
+            binding.textName.text = teamMember.name
+            binding.textDetails.text = teamMember.details
         }
     }
 }
