c3d0adcba80c5a358f56c562c371fdb0d4e06105
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-08-24 09:22:02 +0600

    Some new fragments are added

--- app/src/main/java/com/example/codepiece/fragments/UploadCourseFragment.kt
+++ app/src/main/java/com/example/codepiece/fragments/UploadCourseFragment.kt
@@ -7,12 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.codepiece.R
 
-class UploadCoursesFragment : Fragment() {
+class UploadCourseFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upload_courses, container, false)
+        return inflater.inflate(R.layout.fragment_upload_course, container, false)
     }
+
 }
