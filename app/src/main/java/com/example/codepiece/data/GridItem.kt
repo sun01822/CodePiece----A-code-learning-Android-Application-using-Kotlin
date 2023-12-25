8e217b9922adcf695727c33ccd4d39fd6ecbf2ba
MDShariarHossainSun <mdshariarhossainsun01822@gmail.com>
2023-12-25 15:27:27 +0600

    Language of Quiz frames added

---
+++ app/src/main/java/com/example/codepiece/data/GridItem.kt
@@ -0,0 +1,7 @@
+package com.example.codepiece.data
+
+data class GridItem(
+    val name: String ?=null,
+    val imageUrl: String ?= null,
+)
+
--- app/src/main/java/com/example/codepiece/fragments/JoinContestFragment.kt
+++ app/src/main/java/com/example/codepiece/fragments/JoinContestFragment.kt
@@ -5,27 +5,72 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.codepiece.R
+import com.example.codepiece.adapter.GridAdapter
+import com.example.codepiece.data.GridItem
 import com.example.codepiece.databinding.FragmentJoinContestBinding
+import com.example.codepiece.fragments.languages.CFragment
+import com.example.codepiece.fragments.languages.CPPFragment
+import com.example.codepiece.fragments.languages.JavaFragment
+import com.example.codepiece.fragments.languages.PythonFragment
+import com.example.codepiece.helper.FragmentHelper
 
 class JoinContestFragment : Fragment() {
     private lateinit var binding : FragmentJoinContestBinding
+    private lateinit var gridRecyclerView: RecyclerView
+    private lateinit var gridAdapter: GridAdapter
+    private lateinit var value : String
+    private val gridItems =listOf(
+        GridItem("C programming", "https://res.cloudinary.com/practicaldev/image/fetch/s--jfMWJNMp--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/b22wdroslo9khnoyssmn.png"),
+        GridItem("C++", "https://www.nicepng.com/png/detail/111-1116276_computer-science-i-syllabus-and-grading-policy-c.png"),
+        GridItem("Java", "https://static.vecteezy.com/system/resources/previews/020/111/555/non_2x/java-editorial-logo-free-download-free-vector.jpg"),
+        GridItem("Python", "https://www.pngitem.com/pimgs/m/159-1595932_python-logo-png-transparent-images-logo-transparent-background.png"),
+    )
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentJoinContestBinding.inflate(layoutInflater)
+        gridRecyclerView = binding.gridRecyclerView
+        gridRecyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
+        gridAdapter = GridAdapter(requireContext(), gridItems)
+        gridRecyclerView.adapter = gridAdapter
+        gridAdapter.setOnclickListener(object : GridAdapter.OnClickListener{
+            override fun onClick(position: Int, gridItem: GridItem) {
+                val fragment = when (position) {
+                    0 -> {
+                        value = gridItem.name.toString()
+                        FragmentHelper.getFragmentWithValue(CFragment(),value)
+                    }
+                    1 -> {
+                        value = gridItem.name.toString()
+                        FragmentHelper.getFragmentWithValue(CPPFragment(),value)
+                    }
+                    2 -> {
+                        value = gridItem.name.toString()
+                        FragmentHelper.getFragmentWithValue(JavaFragment(),value)
+                    }
+                    3 -> {
+                        value = gridItem.name.toString()
+                        FragmentHelper.getFragmentWithValue(PythonFragment(),value)
+                    }
+                    else -> {
+                        null
+                    }
+                }
+                parentFragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainer, fragment!!)
+                    .addToBackStack(null)
+                    .commit()
+            }
+
+            override fun onClick(p0: View?) {
+
+            }
+        })
         return binding.root
-
-
-
-
-
-
-
-
-
-
     }
 }
